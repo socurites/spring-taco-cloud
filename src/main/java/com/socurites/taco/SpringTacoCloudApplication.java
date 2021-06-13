91e9f795f2495997db08832e65d1999fa66870b6
Junyi Song <junyisong@this.local>
2021-06-13 20:56:00 +0900

    First commit

---
+++ src/main/java/com/socurites/taco/SpringTacoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.socurites.taco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTacoCloudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/socurites/taco/model/Taco.java
@@ -0,0 +1,11 @@
+package com.socurites.taco.model;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Taco {
+	private String name;
+	private List<String> ingredients;
+}
