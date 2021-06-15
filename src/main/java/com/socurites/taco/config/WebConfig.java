f2386c0792c750daea80d4d68bd7bf84a68bbdea
Junyi Song <junyisong@this.local>
2021-06-15 23:46:16 +0900

    chap2

---
+++ src/main/java/com/socurites/taco/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.socurites.taco.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		WebMvcConfigurer.super.addViewControllers(registry);
+		
+		registry.addViewController("/test-path").setViewName("home");
+		registry.addRedirectViewController("/test-path2", "/");
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/socurites/taco/controller/OrderController.java
@@ -0,0 +1,31 @@
+package com.socurites.taco.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.socurites.taco.model.Order;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+	@GetMapping("/current")
+	public String orderForm(Model model) {
+		model.addAttribute("order", new Order());
+		
+		return "orderForm";
+	}
+	
+	@PostMapping
+	public String processOrder(Order order) {
+		log.info("Order submitted: " + order);
+		
+		return "redirect:/";
+	}
+	
+}
