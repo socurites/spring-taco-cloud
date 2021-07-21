4a0a81c8e1ffa27998f3f1d285e6de054474a25f
Junyi Song <junyisong@this.local>
2021-07-21 16:31:02 +0900

    Remove redis session

--- src/main/java/com/socurites/taco/SpringTacoCloudApplication.java
+++ src/main/java/com/socurites/taco/SpringTacoCloudApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession
+//@EnableRedisHttpSession
 public class SpringTacoCloudApplication {
 
 	public static void main(String[] args) {
